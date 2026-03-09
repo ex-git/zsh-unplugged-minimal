d2a5bbf2a4c048f9759d76862dff4e4d06482b3b
Evan Xu <hanbaoxu@gmail.com>
2026-03-09 07:43:38 -0700

    Add zsh command completions for nvm, pyenv, and uv

--- zsh_functions/nvm.zsh
+++ zsh_functions/nvm.zsh
@@ -43,3 +43,36 @@ nvm() {
   _nvm_load
   nvm "$@"
 }
+
+# Zsh completion for nvm
+_nvm_zsh_complete() {
+  local -a commands
+  commands=(
+    'install:Install a specific Node.js version'
+    'use:Switch to use a specific version'
+    'run:Run a command with a specific Node version'
+    'exec:Execute a command with a specific Node version'
+    'current:Show the currently active version'
+    'ls:List installed versions'
+    'list:List installed versions'
+    'list-remote:List available versions to install'
+    'ls-remote:List available versions to install'
+    'install-latest-npm:Install latest npm for current node'
+    'reinstall-packages:Reinstall global packages'
+    'uninstall:Uninstall a specific version'
+    'alias:Manage version aliases'
+    'unalias:Remove a version alias'
+    'load:NVM load'
+    'unload:NVM unload'
+    'shutdown:Shutdown NVM and clear environment'
+    'reset:Clear NVM directory'
+    'version:Show NVM version'
+    'version-remote:Show remote version'
+    'help:Show help'
+  )
+
+  _describe 'command' commands
+}
+
+# Register completion (silently fails if compinit not loaded yet)
+compdef _nvm_zsh_complete nvm 2>/dev/null || true
